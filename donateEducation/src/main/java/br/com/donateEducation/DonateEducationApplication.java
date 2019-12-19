2aadfa8fe7a7eef78995b232ac533281381e3b46
Samuel Oliveira <Samuel Oliveira>
2019-12-18 21:44:06 -0300

    Fisrt commit

---
+++ donateEducation/src/main/java/br/com/donateEducation/DonateEducationApplication.java
@@ -0,0 +1,15 @@
+package br.com.donateEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonateEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonateEducationApplication.class, args);
+		
+		System.out.println("Test donate Education...");
+	}
+
+}
---
+++ donateEducation/src/test/java/br/com/donateEducation/DonateEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.donateEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonateEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
